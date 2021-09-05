28c7ca3a7d099ab1e0d3c6c0301846774d2432ec
Benjamin Smith <bravosierra99@gmail.com>
2021-09-05 12:54:10 -0400

    ok a slightly better way to have a default message

--- simple-status-web/src/components/Status/StatusGrid.js
+++ simple-status-web/src/components/Status/StatusGrid.js
@@ -9,20 +9,27 @@ function StatusGrid(props) {
   // }
 
   useEffect(() => {
-  fetch("http://localhost/api/components/statuses")
-    .then((statuses) => {
-      return statuses.json();
-    })
-    .then((data) => {
-      let StatusObjects = data.map((status) => {
-        return <Status {...status} />;
-      });
-      
-      setStatuses(StatusObjects);
-    });
+    fetch("http://localhost/api/components/statuses")
+      .then((statuses) => {
+        return statuses.json();
+      })
+      .then((data) => {
+        let StatusObjects = data.map((status) => {
+          return <Status {...status} />;
+        });
+        if (StatusObjects.length === 0) {
+          StatusObjects = <h3>No Statuses have been submitted yet</h3>;
+        }
 
-  },[])
-  const [Statuses, setStatuses] = useState(<h3>No Statuses have been submitted yet</h3>);
+        setStatuses(StatusObjects);
+      });
+  }, []);
+  const [Statuses, setStatuses] = useState(
+    <h3>
+      Fetching Data from Server, This message should only be up momentarily. In
+      fact if you are reading this something probably wrong
+    </h3>
+  );
 
   const statuses = props.statuses
     ? props.statuses.map((status) => <Status {...status} />)
